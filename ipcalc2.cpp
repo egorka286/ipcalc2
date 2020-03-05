112b24554ce456205f4f8122373d2116ed47f446
Егор <egorka286@gmail.com>
2020-03-05 16:24:46 +0300

    2

--- ipcalc2.cpp
+++ ipcalc2.cpp
@@ -5,15 +5,24 @@
 #include <stdio.h>
 #include "resource.h"
 
-union net
+union ip_u
 {
-	DWORD full;
+	DWORD adr;
 	BYTE oct[4];
 };
+
+struct net
+{
+	union ip_u ip;
+	union ip_u mask;
+};
+
+typedef struct net NET;
 
 BOOL CALLBACK DialogProc(HWND, UINT, WPARAM, LPARAM);
 TCHAR* dec(TCHAR* Buf, DWORD n);
 TCHAR* bin(TCHAR* Buf, DWORD n);
+DWORD get_mask(DWORD hosts);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpszCmdLine, int nCmdShow)
 {
@@ -22,106 +31,170 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpszCmdLine, 
 
 BOOL CALLBACK DialogProc(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam)
 {
-	static HWND out;
-	static HWND in_mask;
-	static HWND in_ip;
+	static HWND hOutText;
+	static HWND hOutDec;
+	static HWND hOutBin;
+	static HWND hInMask;
+	static HWND hInIP;
+	static HWND hFullIP;
+	static HWND hFullMask;
 	TCHAR ip_buf[16];
-	TCHAR out_buf[25500];
+	TCHAR out_buf[216];
 	TCHAR out_tmp[64];
-	TCHAR tmp1[16];
-	TCHAR tmp2[36];
+	TCHAR dstr[16];
+	TCHAR bstr[36];
 	int net_prefix;
-	union net ip;
-	union net mask;
-	union net net_name;
-	union net net_broadcast;
+	NET net0;
+	union ip_u net_name;
+	union ip_u net_broadcast;
 
 	switch (uMessage)
 	{
 	case WM_INITDIALOG:
-		out = GetDlgItem(hWnd, IDC_STATIC1);
-		in_mask = GetDlgItem(hWnd, IDC_COMBO1);
-		in_ip = GetDlgItem(hWnd, IDC_IPADDRESS1);
+		hOutText = GetDlgItem(hWnd, IDC_OUT_TEXT);
+		hOutDec = GetDlgItem(hWnd, IDC_OUT_DEC);
+		hOutBin = GetDlgItem(hWnd, IDC_OUT_BIN);
+		hInMask = GetDlgItem(hWnd, IDC_INFO_MASK);
+		hInIP = GetDlgItem(hWnd, IDC_INFO_IP);
+		hFullMask = GetDlgItem(hWnd, IDC_FULL_MASK);
 
-		SetWindowText(in_ip, TEXT("192.168.0.1"));
+		SetWindowText(hInIP, TEXT("192.168.0.1"));
 
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/32 255.255.255.255")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/31 255.255.255.254")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/30 255.255.255.252")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/29 255.255.255.248")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/28 255.255.255.240")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/27 255.255.255.224")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/26 255.255.255.192")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/25 255.255.255.128")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/24 255.255.255.0")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/23 255.255.254.0")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/22 255.255.252.0")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/21 255.255.248.0")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/20 255.255.240.0")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/19 255.255.224.0")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/18 255.255.192.0")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/17 255.255.128.0")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/16 255.255.0.0")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/15 255.254.0.0")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/14 255.252.0.0")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/13 255.248.0.0")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/12 255.240.0.0")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/11 255.224.0.0")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/10 255.192.0.0")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/9 255.128.0.0")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/8 255.0.0.0")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/7 254.0.0.0")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/6 252.0.0.0")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/5 248.0.0.0")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/4 240.0.0.0")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/3 224.0.0.0")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/2 192.0.0.0")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/1 128.0.0.0")));
-		SendMessage(in_mask, CB_ADDSTRING, 0, LPARAM(TEXT("/0 0.0.0.0")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /32 255.255.255.255")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /31 255.255.255.254")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /30 255.255.255.252")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /29 255.255.255.248")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /28 255.255.255.240")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /27 255.255.255.224")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /26 255.255.255.192")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /25 255.255.255.128")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /24 255.255.255.0")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /23 255.255.254.0")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /22 255.255.252.0")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /21 255.255.248.0")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /20 255.255.240.0")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /19 255.255.224.0")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /18 255.255.192.0")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /17 255.255.128.0")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /16 255.255.0.0")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /15 255.254.0.0")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /14 255.252.0.0")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /13 255.248.0.0")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /12 255.240.0.0")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /11 255.224.0.0")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /10 255.192.0.0")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /9 255.128.0.0")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /8 255.0.0.0")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /7 254.0.0.0")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /6 252.0.0.0")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /5 248.0.0.0")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /4 240.0.0.0")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /3 224.0.0.0")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /2 192.0.0.0")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /1 128.0.0.0")));
+		SendMessage(hInMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /0 0.0.0.0")));
 
-		SendMessage(in_mask, CB_SETCURSEL, 8, 0);
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /32 255.255.255.255")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /31 255.255.255.254")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /30 255.255.255.252")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /29 255.255.255.248")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /28 255.255.255.240")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /27 255.255.255.224")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /26 255.255.255.192")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /25 255.255.255.128")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /24 255.255.255.0")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /23 255.255.254.0")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /22 255.255.252.0")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /21 255.255.248.0")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /20 255.255.240.0")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /19 255.255.224.0")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /18 255.255.192.0")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /17 255.255.128.0")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /16 255.255.0.0")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /15 255.254.0.0")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /14 255.252.0.0")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /13 255.248.0.0")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /12 255.240.0.0")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /11 255.224.0.0")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /10 255.192.0.0")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /9 255.128.0.0")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /8 255.0.0.0")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /7 254.0.0.0")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /6 252.0.0.0")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /5 248.0.0.0")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /4 240.0.0.0")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /3 224.0.0.0")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /2 192.0.0.0")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /1 128.0.0.0")));
+		SendMessage(hFullMask, CB_ADDSTRING, 0, LPARAM(TEXT(" /0 0.0.0.0")));
+
+		SendMessage(hInMask, CB_SETCURSEL, 8, 0);
+
+		SendMessage(hWnd, WM_NET_INFO, 0, 0);
 
 		return TRUE;
 
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
-		case IDC_BUTTON1:
-			GetWindowText(in_ip, ip_buf, sizeof(ip_buf));
-			_stscanf(ip_buf, TEXT("%hhu.%hhu.%hhu.%hhu"), &ip.oct[3], &ip.oct[2], &ip.oct[1], &ip.oct[0]);
+		case IDC_INFO_MASK:
+			if (HIWORD(wParam) == CBN_SELCHANGE)
+			SendMessage(hWnd, WM_NET_INFO, 0, 0);
+			break;
 
-			net_prefix = SendMessage(in_mask, CB_GETCURSEL, 0, 0);
-			mask.full = 0xFFFFFFFF << net_prefix;
-
-			net_name.full = ip.full & mask.full;
-			net_broadcast.full = net_name.full + ~mask.full;
-
-			wsprintf(out_buf, TEXT("IP\t\t%s \t%s\n"), dec(tmp1, ip.full), bin(tmp2, ip.full));
-
-			wsprintf(out_tmp, TEXT("Bitmask\t\t%d\n"), 32 - net_prefix);
-			lstrcat(out_buf, out_tmp);
-
-			wsprintf(out_tmp, TEXT("Netmask\t%s \t%s\n"), dec(tmp1, mask.full), bin(tmp2, mask.full));
-			lstrcat(out_buf, out_tmp);
-
-			wsprintf(out_tmp, TEXT("Network\t%s \t%s\n"), dec(tmp1, net_name.full), bin(tmp2, net_name.full));
-			lstrcat(out_buf, out_tmp);
-
-			wsprintf(out_tmp, TEXT("Hostmin\t\t%s \t%s\n"), dec(tmp1, net_name.full + 1), bin(tmp2, net_name.full + 1));
-			lstrcat(out_buf, out_tmp);
-
-			wsprintf(out_tmp, TEXT("Hostmax\t%s \t%s\n"), dec(tmp1, net_broadcast.full - 1), bin(tmp2, net_broadcast.full - 1));
-			lstrcat(out_buf, out_tmp);
-
-			wsprintf(out_tmp, TEXT("Broadcast\t%s \t%s\n"), dec(tmp1, net_broadcast.full), bin(tmp2, net_broadcast.full));
-			lstrcat(out_buf, out_tmp);
-
-			wsprintf(out_tmp, TEXT("Hosts\t\t%lu\n"), ~mask.full - 1);
-			lstrcat(out_buf, out_tmp);
-
-			SetWindowText(out, out_buf);
+		case IDC_INFO_IP:
+			if (HIWORD(wParam) == EN_CHANGE)
+				SendMessage(hWnd, WM_NET_INFO, 0, 0);
 			break;
 		}
+		return TRUE;
+
+	case WM_NET_INFO:
+		GetWindowText(hInIP, ip_buf, 16);
+		_stscanf(ip_buf, TEXT("%hhu.%hhu.%hhu.%hhu"), &net0.ip.oct[3], &net0.ip.oct[2], &net0.ip.oct[1], &net0.ip.oct[0]);
+
+		net_prefix = SendMessage(hInMask, CB_GETCURSEL, 0, 0);
+		net0.mask.adr = 0xFFFFFFFF << net_prefix;
+
+		net_name.adr = net0.ip.adr & net0.mask.adr;
+		net_broadcast.adr = net_name.adr + ~net0.mask.adr;
+
+		wsprintf(out_buf, TEXT("IP адрес\nПрефикс\nМаска\nИмя сети\nПервый IP\nПоследний IP\nBroadcast\nЧисло хостов"));
+		SetWindowText(hOutText, out_buf);
+
+		wsprintf(out_buf, TEXT("%s\n"), dec(dstr, net0.ip.adr));
+		wsprintf(out_tmp, TEXT("%d\n"), 32 - net_prefix);
+		lstrcat(out_buf, out_tmp);
+		wsprintf(out_tmp, TEXT("%s\n"), dec(dstr, net0.mask.adr));
+		lstrcat(out_buf, out_tmp);
+		wsprintf(out_tmp, TEXT("%s\n"), dec(dstr, net_name.adr));
+		lstrcat(out_buf, out_tmp);
+		wsprintf(out_tmp, TEXT("%s\n"), dec(dstr, net_name.adr + 1));
+		lstrcat(out_buf, out_tmp);
+		wsprintf(out_tmp, TEXT("%s\n"), dec(dstr, net_broadcast.adr - 1));
+		lstrcat(out_buf, out_tmp);
+		wsprintf(out_tmp, TEXT("%s\n"), dec(dstr, net_broadcast.adr));
+		lstrcat(out_buf, out_tmp);
+		wsprintf(out_tmp, TEXT("%lu\n"), ~net0.mask.adr - 1);
+		lstrcat(out_buf, out_tmp);
+		SetWindowText(hOutDec, out_buf);
+
+		wsprintf(out_buf, TEXT("%s\n"), bin(bstr, net0.ip.adr));
+		wsprintf(out_tmp, TEXT("\n"));
+		lstrcat(out_buf, out_tmp);
+		wsprintf(out_tmp, TEXT("%s\n"), bin(bstr, net0.mask.adr));
+		lstrcat(out_buf, out_tmp);
+		wsprintf(out_tmp, TEXT("%s\n"), bin(bstr, net_name.adr));
+		lstrcat(out_buf, out_tmp);
+		wsprintf(out_tmp, TEXT("%s\n"), bin(bstr, net_name.adr + 1));
+		lstrcat(out_buf, out_tmp);
+		wsprintf(out_tmp, TEXT("%s\n"), bin(bstr, net_broadcast.adr - 1));
+		lstrcat(out_buf, out_tmp);
+		wsprintf(out_tmp, TEXT("%s\n"), bin(bstr, net_broadcast.adr));
+		lstrcat(out_buf, out_tmp);
+		wsprintf(out_tmp, TEXT("\n"));
+		lstrcat(out_buf, out_tmp);
+		SetWindowText(hOutBin, out_buf);
 		return TRUE;
 
 	case WM_CLOSE:
@@ -133,8 +206,8 @@ BOOL CALLBACK DialogProc(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam)
 
 TCHAR* dec(TCHAR* Buf, DWORD n)
 {
-	union net tmp;
-	tmp.full = n;
+	union ip_u tmp;
+	tmp.adr = n;
 	wsprintf(Buf, TEXT("%d.%d.%d.%d"), tmp.oct[3], tmp.oct[2], tmp.oct[1], tmp.oct[0]);
 	return Buf;
 }
@@ -145,8 +218,27 @@ TCHAR* bin(TCHAR* Buf, DWORD n)
 	for (DWORD i = 0x80000000; i > 0; i >>= 1)
 	{
 		Buf[r++] = (n & i) ? '1' : '0';
-		if (i == 0x01000000 | i == 0x10000 | i == 0x100)
+		if ((i == 0x01000000) | (i == 0x10000) | (i == 0x100))
 			Buf[r++] = '.';
 	}
+	Buf[r] = 0;
 	return Buf;
+}
+
+DWORD get_mask(DWORD hosts)
+{
+	int i = 0;
+	hosts += 2;
+	while (hosts != 1)
+	{
+		hosts >>= 1;
+		i++;
+	}
+	while (i > 0)
+	{
+		hosts <<= 1;
+		hosts |= 1;
+		i--;
+	}
+	return ~hosts;
 }
--- resource.h
+++ resource.h
@@ -1,23 +1,21 @@
-//{{NO_DEPENDENCIES}}
-// Включаемый файл, созданный в Microsoft Visual C++.
-// Используется ipcalc2.rc
-//
-#define IDD_DIALOG1                     101
-#define IDC_IPADDRESS1                  1001
-#define IDC_NETADDRESS1                 1002
-#define IDC_IPADDRESS2                  1003
-#define IDC_LIST1                       1004
-#define IDC_BUTTON1                     1005
-#define IDC_STATIC1						1006
-#define IDC_COMBO1                      1007
+#define IDD_DIALOG1					101
 
-// Next default values for new objects
-// 
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1008
-#define _APS_NEXT_SYMED_VALUE           101
-#endif
-#endif
+#define IDC_INFO_GROUP				1001
+#define IDC_INFO_TEXT_IP			1002
+#define IDC_INFO_IP					1003
+#define IDC_INFO_TEXT_MASK			1004
+#define IDC_INFO_MASK				1005
+#define IDC_OUT_TEXT				1006
+#define IDC_OUT_DEC					1007
+#define IDC_OUT_BIN					1008
+#define IDC_CALC_GROUP				1009
+#define IDC_FULL_GROUP				1010
+#define IDC_FULL_TEXT_IP			1011
+#define IDC_FULL_IP					1012
+#define IDC_FULL_TEXT_MASK			1013
+#define IDC_FULL_MASK				1014
+#define IDC_ADD_BUTTON				1015
+#define IDC_DEL_BUTTON				1016
+#define IDC_NET_LIST				1017
+
+#define WM_NET_INFO					WM_USER + 0x100
